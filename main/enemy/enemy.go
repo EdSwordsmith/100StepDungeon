9c5c4ba389fe90dfc8c9a0c97ed6e89680d2db6e
Eduardo Espadeiro <edu.espadeiro@gmail.com>
2019-06-12 18:35:50 +0100

    Collection proxies and basic enemy follow

---
+++ main/enemy/enemy.go
@@ -0,0 +1,35 @@
+components {
+  id: "enemy"
+  component: "/main/enemy/enemy.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/map/tiles.tilesource\"\n"
+  "default_animation: \"enemy\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
