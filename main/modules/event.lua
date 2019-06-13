9e428e89d0b466cfdb84e92a2b8a14069cac1a4f
Eduardo Espadeiro <edu.espadeiro@gmail.com>
2019-06-13 17:41:52 +0100

    Ready for release 2

--- main/modules/event.lua
+++ main/modules/event.lua
@@ -1,10 +1,15 @@
-local function Event(callback)
+local function Event(callback, instance)
 	local object = {}
 	object.callback = callback
+	object.instance = instance
 
 	function object:handle_messages(message_id, message, sender)
 		if message_id == hash("handle_event") then
-			self.callback()
+			if instance ~= nil then
+				self.callback(instance)
+			else
+				self.callback()
+			end
 		end
 	end
 
